b5bc81a8977e4ef016388975aa87ef14fa761916
Niss337 <nissrineelkhatib01@gmail.com>
2025-01-06 10:34:58 +0100

    Update constantes.h

--- constantes.h
+++ constantes.h
@@ -1,13 +1,17 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define SUCCESS 0
-#define ERROR 1
-#define HOST_ARG_INDEX 1
-#define FILE_ARG_INDEX 2
+#define TFTP_PORT "1069"      // TFTP port to connect to
+#define BUFFER_SIZE 516        // Maximum size of TFTP packets
+#define TIMEOUT_SEC 5          // Timeout for receiving packets
+
 
 #define USAGE_MSG "Usage: %s <host> <file>\n"
-#define ADDRESS_OBTAINED_MSG "Address obtained for host.\n"
-#define SOCKET_CREATED_MSG "Socket created successfully.\n"
+#define ERROR_OPENING_FILE "Error opening file for writing\n"
+#define RECV_BLOCK_MSG "Received block %d\n"
+#define FILE_TRANSFER_COMPLETE "File transfer complete.\n"
+#define GETADDRINFO_ERROR "getaddrinfo\n"
+#define SOCKET_ERROR "socket\n"
+#define RECVFROM_ERROR "recvfrom\n"
 
-#endif 
+#endif // CONSTANTS_H
